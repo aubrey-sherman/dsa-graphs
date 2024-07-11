cfebff7612268d23e022580d9d0580f663902633
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-07-11 15:39:52 -0400

    Implement UGraph class

--- graphs/graph/graph.ts
+++ graphs/graph/graph.ts
@@ -21,22 +21,42 @@ class UGraphStr {
 
   /** Add node to graph. */
   addNode(node: UGraphNodeStr): void {
+    this.nodes.add(node);
   }
 
   /** Add array of nodes to graph. */
   addNodes(nodeArray: UGraphNodeStr[]): void {
+    for (const node of nodeArray) {
+      this.addNode(node);
+    }
   }
 
   /** Add edge between v1 and v2. */
   addEdge(v1: UGraphNodeStr, v2: UGraphNodeStr): void {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  /** Add edges between vertices. */
+  addEdges(vArray: [UGraphNodeStr, UGraphNodeStr][]): void {
+    for (const [v1, v2] of vArray) {
+      this.addEdge(v1, v2);
+    }
   }
 
   /** Remove edge between v1 and v2. */
   removeEdge(v1: UGraphNodeStr, v2: UGraphNodeStr): void {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
   }
 
   /** Remove node from graph. */
   removeNode(node: UGraphNodeStr): void {
+    for (const adj of node.adjacent) {
+      this.removeEdge(node, adj);
+    }
+
+    this.nodes.delete(node);
   }
 }
 
