b6b5ebdfcdd471241d7a81ea40ba7abcb8c62583
Anya Aven <leafeon96@gmail.com>
2024-07-11 18:06:03 -0400

    Finish rDfs

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,12 +1,39 @@
 import { UGraphNodeStr } from "../graph/graph";
 
+
+/*
+For each call of rDfs, result is the SAME reference as the original.
+As we push to it per call, it is adding to the most outer function call's
+reference. This works because each call has variable block scope.
+
+     d -- b -- a -- c
+      \------------/
+1(a) result: [] visited: [a]    - adjacents: [b, c]
+  2(b) result: [a] visited: [a, b]  - adjacents: [d, a]
+    3(d) result: [a, b] visited: [a, b, d]  - adjacents: [b, c]
+     4(c) result: [a, b, d] visited: [a, b, d, c]  - adjacents: [a, d]
+
+    final product-> [a, b, d, c]
+
+*/
+
 /** Return array of nodes, in DFS order (recursive version)  */
 
 function rDfs(
-    start: UGraphNodeStr,
-    result: UGraphNodeStr[] = [],
-    visited = new Set([start])): UGraphNodeStr[] {
-  return ["todo"];
+  start: UGraphNodeStr,
+  result: UGraphNodeStr[] = [],
+  visited = new Set([start])): UGraphNodeStr[] {
+
+  result.push(start);
+
+  for (const adj of start.adjacent) {
+    if (!visited.has(adj)) {
+      visited.add(adj);
+      rDfs(adj, result, visited);
+    }
+  }
+
+  return result;
 }
 
 /** Return array of nodes, in DFS order (iterative version)  */
